d4ff81360e6df7fae1b446c9d7dc467c772a7344
agugliotta <agugliotta@wustl.edu>
2017-07-25 12:55:07 -0400

    Java - edits

--- Java_2/app.js
+++ Java_2/app.js
@@ -1,40 +1,28 @@
-/* Boolean values */
+var firstNumber = parseInt( prompt('Please enter a number') ) || 0;
+var secondNumber = parseInt( prompt('Please enter a second number') ) || 0;
+var operation = prompt('Please enter a math operation:  Add, Subtract, Multiply, Divide, Exponent').toLowerCase();
 
-var firstNumber = parseInt (promt("Enter a number please"));  
-var secondNumber = parseInt (promt("Enter a second number please"));
-
-var operation = prompt("Please enter a math operation: Add, Subtract, Multiply, Divide, Exponent").toLowerCase();
-
-if(operation == "Add"); 
-
-	{	console.log( firstNumber + secondNumber ); }
-
-else if(operation == "Subtract"); {
-		
-		console.log( firstNumber - secondNumber ); }
-
-else if(operation == "Multiply"); {
-		 
-		 console.log( firstNumber * secondNumber ); }
-
-
-else if(operation == "divide" && secondNumber == 0); {
-  
-  if(secondNumber == 0); {
-    
-    console.log("Divide"); }
-
- else {
-    console.log( firstNumber / secondNumber ); }
-
-else if(operation == "Exponent"); {
+if(operation == "add") {
 	
-
-	console.log( Math.pow(firstNumber, secondNumber) ); }
-
-
-
-
-
-
-
+	console.log( firstNumber + secondNumber );
+} else if(operation == "subtract") {
+	
+	console.log( firstNumber - secondNumber );
+} else if(operation == "multiply") {
+	
+	console.log( firstNumber * secondNumber );
+} else if(operation == "divide" && secondNumber == 0) {
+  if(secondNumber == 0) {
+    
+    console.log("Divide");
+  } else {
+   
+    console.log( firstNumber / secondNumber );
+  }
+} else if(operation == "exponent") {
+	
+	console.log( Math.pow(firstNumber, secondNumber) );
+} else {
+	
+	console.log("Your math is bad and you should feel bad");
+}
