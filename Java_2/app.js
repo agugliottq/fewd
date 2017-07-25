b72657ee5de792bdf74230639f1a021ab2da363a
agugliotta <agugliotta@wustl.edu>
2017-07-25 12:53:36 -0400

    java

--- Java_2/app.js
+++ Java_2/app.js
@@ -5,34 +5,34 @@ var secondNumber = parseInt (promt("Enter a second number please"));
 
 var operation = prompt("Please enter a math operation: Add, Subtract, Multiply, Divide, Exponent").toLowerCase();
 
-if(operation == "Add") 
+if(operation == "Add"); 
 
 	{	console.log( firstNumber + secondNumber ); }
 
-else if(operation == "Subtract") {
+else if(operation == "Subtract"); {
 		
 		console.log( firstNumber - secondNumber ); }
 
-else if(operation == "Multiply") {
+else if(operation == "Multiply"); {
 		 
 		 console.log( firstNumber * secondNumber ); }
 
 
-else if(operation == "divide" && secondNumber == 0) {
+else if(operation == "divide" && secondNumber == 0); {
   
-  if(secondNumber == 0) {
+  if(secondNumber == 0); {
     
     console.log("Divide"); }
 
  else {
     console.log( firstNumber / secondNumber ); }
 
-else if(operation == "Exponent") {
+else if(operation == "Exponent"); {
 	
 
 	console.log( Math.pow(firstNumber, secondNumber) ); }
 
-	
+
 
 
 
