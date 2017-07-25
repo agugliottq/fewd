bb6a54684f8e43f6cc87a9a40c20976594b47943
agugliotta <agugliotta@wustl.edu>
2017-07-25 12:42:19 -0400

    java 2

---
+++ Java_2/app.js
@@ -0,0 +1,4 @@
+var firstNumber = parseInt (promt("Enter a number please")) || 0; /* Boolean values */
+var secondNumber = parseInt (promt("Enter a second number please")) || 0;
+
+var operation = prompt("Please enter a math operation: Adding, subtracting, Multiply, divide, Exponent").toLowerCase();
