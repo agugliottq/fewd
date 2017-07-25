eeefabaa23d71d9ab6c2a003773e99688f7a6136
agugliotta <agugliotta@wustl.edu>
2017-07-25 12:49:55 -0400

    JAVA

--- Java_2/app.js
+++ Java_2/app.js
@@ -1,4 +1,28 @@
-var firstNumber = parseInt (promt("Enter a number please")) || 0; /* Boolean values */
-var secondNumber = parseInt (promt("Enter a second number please")) || 0;
+/* Boolean values */
 
-var operation = prompt("Please enter a math operation: Adding, subtracting, Multiply, divide, Exponent").toLowerCase();
+var firstNumber = parseInt (promt("Enter a number please"));  
+var secondNumber = parseInt (promt("Enter a second number please"));
+
+var operation = prompt("Please enter a math operation: Add, Subtract, Multiply, Divide, Exponent").toLowerCase();
+
+if(operation == "Add") 
+
+	{	console.log( firstNumber + secondNumber ); }
+
+else if(operation == "Subtract") {
+		
+		console.log( firstNumber - secondNumber ); }
+
+else if(operation == "Multiply") {
+		 
+		 console.log( firstNumber * secondNumber ); }
+
+else if(operation == "multiply") {
+	
+	console.log( firstNumber * secondNumber ); }
+
+
+
+
+
+
